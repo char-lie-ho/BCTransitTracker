c49049e73b9b03b897bd5f51ae5b8871d9b39b2a
ho921 <tho124@my.bcit.ca>
2023-12-22 12:09:34 -0800

    Display bus route on infoWindow

--- backend/script.js
+++ backend/script.js
@@ -46,13 +46,13 @@ function displayResult(data, type, route) {
         data.forEach(function (element) {
             resultList.push({
                 lat: element['Latitude'], lng: element['Longitude'], routes: element['Routes'],
-                StopNo: element['StopNo']
+                StopNo: element['StopNo'], Name: element['Name']
             })
         })
         displayStops(resultList)
     }
 
-    console.log(resultList)
+    // console.log(resultList)
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -72,13 +72,19 @@ function initMap() {
         fetchData('initial', pos)
         new google.maps.Map(document.getElementById('map'), {
             center: pos,
-            zoom: 14
+            zoom: 14,
+            mapTypeControl: false,
+            streetViewControl: false,
         })
     })
 }
 
 function displayRoute(busLocations, routeNo) {
-    var map = new google.maps.Map(document.getElementById('map'));
+    var map = new google.maps.Map(document.getElementById('map'), {
+        mapTypeControl: false,
+        streetViewControl: false,
+        zoomControl: false,
+    });
 
     var kmlLayer = new google.maps.KmlLayer({
         url: `https://nb.translink.ca/geodata/${routeNo}.kmz`
@@ -97,7 +103,11 @@ function displayRoute(busLocations, routeNo) {
     })
 }
 
-function displayStops(busStops) {
+async function displayStops(busStops) {
+    const { Map, InfoWindow } = await google.maps.importLibrary("maps");
+    const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
+        "marker",
+    );
 
     navigator.geolocation.getCurrentPosition((position) => {
         const pos = {
@@ -107,17 +117,39 @@ function displayStops(busStops) {
         const map = new google.maps.Map(document.getElementById("map"), {
             zoom: 15,
             center: pos,
+            mapId: "4504f8b37365c3d0",
+            mapTypeControl: false,
+            streetViewControl: false,
+            zoomControl: false,
         })
         console.log(busStops)
-        const image = "image/stop.png"
-        busStops.forEach(stop => {
-            const marker = new google.maps.Marker({
+
+        // Create an info window to share between markers.
+        const infoWindow = new InfoWindow();
+
+
+
+        // Create the markers.
+        busStops.forEach((stop) => {
+            const contentString = ` <p>${stop.Name}</p>
+                                    <p>Route: ${stop.routes}</p>`
+            // <p>Stop number: ${stop.StopNo}</p>
+            const marker = new AdvancedMarkerElement({
                 position: { lat: stop.lat, lng: stop.lng },
-                map: map,
-                title: location.title,
-                icon: image,
+                map,
+                title: contentString,
+                // content: stop.Name,
             });
-        },
-        )
+
+            // Add a click listener for each marker, and set up the info window.
+            marker.addListener("click", ({ domEvent, latLng }) => {
+                const { target } = domEvent;
+
+                infoWindow.close();
+                infoWindow.setContent(marker.title);
+                infoWindow.open(marker.map, marker);
+            });
+        });
     })
 }
+
