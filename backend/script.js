ce5dfc39fb8f88f285c3b6b7637ff4ac9eb05016
ho921 <tho124@my.bcit.ca>
2023-12-21 22:13:33 -0800

    Display map when enter bus route

--- backend/script.js
+++ backend/script.js
@@ -1,7 +1,7 @@
-function fetchData(type) {
-    var url;
-    const key = '5sPTi0xZHHUeDbm2V9Hm';
-    const keyAlt = 'TlaRyCg6LXfJnQ48nqJL';
+var url;
+const key = '5sPTi0xZHHUeDbm2V9Hm';
+const keyAlt = 'TlaRyCg6LXfJnQ48nqJL';
+function fetchData(type, pos = null) {
     const headers = new Headers({
         'Accept': 'application/json',
     });
@@ -16,6 +16,9 @@ function fetchData(type) {
     } else if (type === 'estimate') {
         var estimate = document.getElementById("estimate").value;
         url = `https://api.translink.ca/rttiapi/v1/stops/${estimate}/estimates?apikey=${key}`
+    } else if (type === 'initial') {
+        console.log(pos['lat'])
+        url = `https://api.translink.ca/rttiapi/v1/stops?apikey=${key}&lat=${pos['lat']}&long=${pos['lng']}`
     }
     console.log(url)
 
@@ -23,7 +26,6 @@ function fetchData(type) {
         method: 'GET',
         headers: headers,
     };
-
 
     fetch(url, options)
         .then(response => response.json())
@@ -33,7 +35,6 @@ function fetchData(type) {
 
 function displayResult(data, type, route) {
     console.log(data, type)
-    initMap(route)
 
     let busLocations = []
     if (type === 'route') {
@@ -41,10 +42,12 @@ function displayResult(data, type, route) {
         var resultList = document.getElementById('resultList');
         resultList.innerText = ''
         data.forEach(function (element) {
-            busLocations.push([{ lat: element['Latitude'], lng: element['Longitude'], dir: element['Direction'] }])
+            busLocations.push({ lat: element['Latitude'], lng: element['Longitude'], dir: element['Direction'] })
         })
+        displayRoute(busLocations, route)
     }
-    document.getElementById('map').style.display = "block"
+
+
     console.log(busLocations)
 }
 
@@ -52,23 +55,39 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM is ready.')
 })
 
-// to make user to hit enter to search
-function searchOnEnter(event) {
-    // Check if the pressed key is Enter (key code 13)
-    if (event.keyCode === 13) {
-        saveSearchandRedirect();
-    }
+
+function initMap() {
+    navigator.geolocation.getCurrentPosition((position) => {
+        const pos = {
+            lat: parseFloat(position.coords.latitude.toFixed(6)),
+            lng: parseFloat(position.coords.longitude.toFixed(6)),
+        }
+        console.log(pos)
+        // Display nearby bus stops
+
+        fetchData('initial', pos)
+        new google.maps.Map(document.getElementById('map'), {
+            center: pos,
+            zoom: 14
+        })
+    })
 }
 
-function initMap(routeNo) {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 0, lng: 0 },
-        zoom: 2
-    });
+function displayRoute(busLocations, routeNo) {
+    var map = new google.maps.Map(document.getElementById('map'));
 
     var kmlLayer = new google.maps.KmlLayer({
         url: `https://nb.translink.ca/geodata/${routeNo}.kmz`
     });
 
     kmlLayer.setMap(map);
+    const image = "image/bus.png"
+    busLocations.forEach(location => {
+        const marker = new google.maps.Marker({
+            position: { lat: location.lat, lng: location.lng },
+            map: map,
+            title: location.title,
+            icon: image,
+        });
+    })
 }
