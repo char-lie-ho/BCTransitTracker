9da422ae2a374f6996067f3f040389e402b53d35
ho921 <tho124@my.bcit.ca>
2023-12-25 14:52:09 -0800

    Add filter buttons

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,63 @@
-var url;
 const key = '5sPTi0xZHHUeDbm2V9Hm';
 const keyAlt = 'TlaRyCg6LXfJnQ48nqJL';
+const busRoute = new Set()
 
+function userLocation() {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            // convert coordinates to 6 digit 
+            const pos = {
+                lat: parseFloat(position.coords.latitude.toFixed(6)),
+                lng: parseFloat(position.coords.longitude.toFixed(6)),
+            }
+            resolve(pos);
+        },
+            (error) => {
+                reject(error);
+            })
+    })
+}
+
+userLocation()
 
 function fetchData(type, info) {
     const headers = new Headers({
         'Accept': 'application/json',
     });
-    if (type === 'route') {
-        var route = document.getElementById("route").value;
-        url = `https://api.translink.ca/rttiapi/v1/buses?apikey=${key}&routeNo=${route}`
-    }
-    else if (type === 'initial') {
-        url = `https://api.translink.ca/rttiapi/v1/stops?apikey=${key}&lat=${info['lat']}&long=${info['lng']}&radius=250`
-    }
-    else if (type === 'stop') {
-        url = `https://api.translink.ca/rttiapi/v1/stops/${info['stopNo']}/estimates?apikey=${key}&count=3&timeframe=60`
-    }
 
-    console.log(url)
+    userLocation().then((pos) => {
+        if (type === 'route') {
+            var route = document.getElementById("route").value;
+            // Check if the searched route is valid
+            if (busRoute.has(String(route))) {
+                console.log(route)
+                url = `https://api.translink.ca/rttiapi/v1/stops?apikey=${key}&lat=${pos['lat']}&long=${pos['lng']}&radius=250&routeNo=${route}`
+            }
+        }
+        if (type === 'initial') {
+            url = `https://api.translink.ca/rttiapi/v1/stops?apikey=${key}&lat=${pos['lat']}&long=${pos['lng']}&radius=250`
+        }
+        else if (type === 'stop') {
+            url = `https://api.translink.ca/rttiapi/v1/stops/${info['stopNo']}/estimates?apikey=${key}&count=3&timeframe=60`
+        }
 
-    const options = {
-        method: 'GET',
-        headers: headers,
-    };
+        console.log(url)
 
-    fetch(url, options)
-        .then(response => response.json())
-        .then(data => displayResult(data, type, info))
-    // .catch(error => {
-    //     console.error('Error:', error);
-    //     alert("Invalid route number. Please use a valid active route id that is 3 digits, i.e. 003, 590.")
-    // });
+        const options = {
+            method: 'GET',
+            headers: headers,
+        };
+
+        fetch(url, options)
+            .then(response => response.json())
+            .then(data => displayResult(data, type, info))
+        // .catch(error => {
+        //     console.error('Error:', error);
+        //     alert("Invalid route number. Please use a valid active route id that is 3 digits, i.e. 003, 590.")
+        // });
+    })
+
+
 }
 
 function displayResult(data, type, info) {
@@ -57,36 +83,52 @@ function displayResult(data, type, info) {
     else if (type == 'stop' && data.length > 0) {
         console.log(data)
         data.forEach(function (element) {
+            // Add to busRoute set
+            busRoute.add(element['RouteNo'])
+            // Display the routes on webpage
             var expectedCountdownArray = element['Schedules']
                 .map(schedule => schedule['ExpectedCountdown'])
-            document.getElementById('resultList').innerHTML += `<div class="col-6"><div class="card" href='#'>
+            document.getElementById('resultList').innerHTML += `<div class="col-6 busRoute" id='${element['RouteNo']}' ><div class="card" href='#'>
             <div class="card-body"><div class="card-title"><button type="button" class="btn btn-info btn-sm">Route: ${element['RouteNo']}</button> 
             @${info['AtStreet']}   
             <b>${element['Direction'][0]}</b></div>
             <span class="card-subtitle mb-2 text-muted">Next bus (mins): ${expectedCountdownArray.join(', ')}</span></div></div></div>`
         })
-        //   on line 66, change to display station name 
-    }
 
+    }
 }
 
-function userLocation() {
-    navigator.geolocation.getCurrentPosition((position) => {
-        // convert coordinates to 6 digit 
-        const pos = {
-            lat: parseFloat(position.coords.latitude.toFixed(6)),
-            lng: parseFloat(position.coords.longitude.toFixed(6)),
-        }; console.log(pos); fetchData('initial', pos)
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM is ready.');
+    fetchData('initial');
+    setTimeout(() => {
+        displayButtons(busRoute)
+    }, 300);
+})
+
+
+function displayButtons(busRoute) {
+    console.log(busRoute)
+    busRoute.forEach(route => {
+        document.getElementById('btns').innerHTML += `<button type="button" class="btn btn-info btn-sm" onclick="modifyDisplay(${route})">${route}</button>`
+    })
+}
+
+function modifyDisplay(route) {
+    const elements = document.querySelectorAll('.busRoute');
+
+    elements.forEach(element => {
+        if (element.id == route) {
+            element.style.display = 'block';
+        } else {
+            element.style.display = 'none';
+        }
     })
 
 }
 
-userLocation()
 
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOM is ready.')
-
-})
 
 
 
